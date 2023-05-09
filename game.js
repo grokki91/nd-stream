d5c2c70f9d5e0dd0b9674b4d8f184fcba88b7ade
Pavel Chekalin <grokki91@gmail.com>
2023-05-09 17:07:49 +0300

    Add fix

--- game.js
+++ game.js
@@ -2,17 +2,26 @@
 
 const fs = require('fs');
 const path = require('path');
-const yargs = require('yargs/yargs');
-const arg = yargs.hideBin(process.argv)[0];
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+console.log('Угадай 1 или 2 \n');
 const guessDigit = () => {
     const digit = Math.floor((Math.random() * 2) + 1);
     const file = path.join(__dirname, 'result.txt');
     const content = `Загаданное число - ${digit}\n`;
     fs.appendFile(file, content, () => {});
-    if (arg == digit) {
-        console.log('Вы угадали!');
-    }
+    rl.question('', (answer) => {
+        if (answer == digit) {
+            console.log('Вы угадали!');
+            fs.appendFile(file, 'Вы угадали!\n', () => {});
+            return rl.close();
+        }
+        guessDigit();
+    })
 }
 
 guessDigit();
